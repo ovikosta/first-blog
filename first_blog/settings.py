caea6933cb01332126e21fb579be2a298b5787b4
OviKosta <ovikosta@gmail.com>
2017-08-03 00:43:13 +0300

    add allowed hosts

--- first_blog/settings.py
+++ first_blog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'c#+cmre5fm2k%c%*f2$8jurqof=qirwjo*fv10v58%z7qlp@rv'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['ovikosta.python.anywhere.com',]
+ALLOWED_HOSTS = ['ovikosta.pythonanywhere.com',]
 
 
 # Application definition
